460219d81cee221765c4e46bab428219fc0ac671
DavisLinger <1095661456@qq.com>
2020-01-27 17:59:10 +0800

    添加前端

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# -
-一天一口毒鸡汤
+# 一天一口毒鸡汤
+毒鸡汤不考虑来一口？
+## 怎么运行？
+###前置条件
+* 安装node
+* 安装go
+* 安装node的http-server`npm install http-server -g`
+* 安装mysql并运行项目路径下的`goweb.sql`
+###运行
+```bash
+go run server.go
+cd front
+http-server -c-1
+```
--- server.go
+++ server.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"log"
 	"math/rand"
 	"net/http"
-	"os"
-	"runtime"
-	"strings"
 )
 
 func main() {
-	data := InitRead()
-	InitWrite(data)
-	log.Println("write ok!")
-	//Route
+	// Route
 	http.HandleFunc("/", dailyBeautifulSentence)
 	err := http.ListenAndServe(":23333", nil)
 	if err != nil {
@@ -27,8 +19,13 @@ func main() {
 }
 
 func dailyBeautifulSentence(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+	w.Header().Set("content-type", "application/text")             //返回数据格式是text
 	n := rand.Int() % 150
-	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/goweb?charset=utf8")
+	log.Println("request path : ", r.URL.Path)
+	log.Println("local n is : ", n)
+	db, err := sql.Open("mysql", "root:linglinger@tcp(127.0.0.1:3306)/goweb?charset=utf8")
 	if err != nil {
 		log.Println(err)
 		db.Close()
@@ -47,59 +44,15 @@ func dailyBeautifulSentence(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 			return
 		}
-		io.WriteString(w, s)
+		_, err = io.WriteString(w, s)
+		if err != nil {
+			log.Println("err:", err)
+		}
 	}
-	rows.Close()
+	err = rows.Close()
+	if err != nil {
+		log.Println(err)
+	}
 	db.Close()
 
-}
-func InitRead() []string {
-	var sentence = make([]string, 0)
-	//filePath := `H:\DavisLing\Documents\s1.txt`
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	var filename string
-	if runtime.GOOS == "windows" {
-		filename = path + "\\s1.txt"
-	} else {
-		filename = path + "/s1.txt"
-	}
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Println(err)
-	}
-	defer file.Close()
-	buf := bufio.NewReader(file)
-	for {
-		line, err := buf.ReadString('\n')
-		line = strings.TrimSpace(line)
-		sentence = append(sentence, line)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("File read ok!")
-				break
-			} else {
-				fmt.Println("Read file error!", err)
-				return nil
-			}
-		}
-	}
-	return sentence
-}
-func InitWrite(sentence []string) {
-	db, err := sql.Open("mysql", "root:1234@tcp(127.0.0.1:3306)/goweb?charset=utf8")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer db.Close()
-	for _, val := range sentence {
-		_, err := db.Exec("INSERT into davis(sentence) values(?) ", val)
-		if err != nil {
-			log.Println(err)
-		}
-	}
 }
