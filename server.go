fc161c02b4bb64f4b14513f3c1299385f8b91582
DavisLinger <1095661456@qq.com>
2020-02-15 15:42:05 +0800

    拆分函数

--- server.go
+++ server.go
@@ -23,30 +23,33 @@ func dailyBeautifulSentence(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 	w.Header().Set("content-type", "application/text")             //返回数据格式是text
 	n := rand.Intn(150) + 1
-	log.Println("request path : ", r.URL.Path)
-	log.Println("local n is : ", n)
+	data := queryFromDB(n)
+	if data == "" {
+		io.WriteString(w, "nop!!!")
+	} else {
+		io.WriteString(w, data)
+	}
+}
+
+func queryFromDB(n int) string {
+	var s string
 	db, err := sql.Open("mysql", "root:linglinger@tcp(127.0.0.1:3306)/goweb?charset=utf8")
 	if err != nil {
 		log.Println(err)
 		db.Close()
-		return
+		return ""
 	}
 	rows, err := db.Query("select sentence from davis where id=?", n)
 	if err != nil {
 		log.Println(err)
 		db.Close()
-		return
+		return ""
 	}
 	for rows.Next() {
-		var s string
 		err := rows.Scan(&s)
 		if err != nil {
 			log.Println(err)
-			return
-		}
-		_, err = io.WriteString(w, s)
-		if err != nil {
-			log.Println("err:", err)
+			return ""
 		}
 	}
 	err = rows.Close()
@@ -54,5 +57,5 @@ func dailyBeautifulSentence(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 	db.Close()
-
+	return s
 }
